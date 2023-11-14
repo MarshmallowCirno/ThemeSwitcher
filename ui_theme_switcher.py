5156bf126c68b2d0cbd715155cd3fd7ed2fd41a0
MarshmallowCirno <marshmallowcirno@gmail.com>
2023-11-14 22:41:46 +0300

    1.1 update
    Ability to set the name of the theme list sidebar tab in the add-on preferences.
    
    Buttons have been moved to the bottom of the theme list to reduce its horizontal size.
    
    The list is now populated automatically.

--- ui_theme_switcher.py
+++ ui_theme_switcher.py
@@ -16,95 +16,110 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
+import os
+
+import bpy
+from bpy.props import CollectionProperty, IntProperty, StringProperty
+from bpy.types import AddonPreferences, Operator, Panel, PropertyGroup, UIList
+from bpy.utils import register_class, unregister_class
+
 
 bl_info = {
-    "name":         "Theme Switcher",
-    "author":       "MarshmallowCirno",
-    "blender":      (2, 80, 0),
-    "version":      (1, 0),
-    "location":     "3D View > Sidebar > View",
-    "description":  "Quick switching between themes with Ctrl+Wheel",
-    "category":     "Interface"
+    "name": "Theme Switcher",
+    "author": "MarshmallowCirno",
+    "blender": (3, 3, 1),
+    "version": (1, 1),
+    "location": "3D View > Sidebar > View",
+    "description": "Fast switching between themes with Ctrl+Wheel",
+    "category": "Interface",
+    "doc_url": "https://gumroad.com/l/PsTugR",
+    "tracker_url": "https://blenderartists.org/t/theme-switcher/1211787",
 }
 
 
-import bpy, os
-from bpy.props import IntProperty, StringProperty, CollectionProperty, PointerProperty
-from bpy.types import Operator, Panel, PropertyGroup, UIList, Scene
-
-
 class TSWITCH_UL_themes(UIList):
-    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
+    def draw_item(self, context, layout, data, item, icon, active_data, active_property, index):
         layout.label(text=item.name)
 
 
-class TSWITH_OT_reload(Operator):
+class TSWITCH_OT_reload(Operator):
     bl_idname = "theme_switcher.reload"
     bl_label = "Reload Themes"
     bl_description = "Reload list of themes"
     bl_options = {'INTERNAL'}
-  
-    def invoke(self, context, event):
-        scene = context.scene
-        themes = scene.theme_switcher.themes
-        
-        # backup active theme name
-        if scene.theme_switcher.themes:
-            active_theme_index = scene.theme_switcher.active_theme_index
-            init_active_theme_name = themes[active_theme_index].name
-        else:
-            init_active_theme_name = ""
-        
-        # clear theme collection
-        scene.theme_switcher.themes.clear()
-        
-        # get theme xmls
-        theme_dir_pathes = bpy.utils.preset_paths("interface_theme")
-        xmls = []
-        for dir_path in theme_dir_pathes:
-            xmls.extend(self.get_xmls(scene, dir_path))
-        xmls.sort(key=lambda x: x[0])
-        
-        # add themes to collection
-        self.import_themes(scene, xmls)
-        
-        # restore active theme
-        if init_active_theme_name:
-            init_active_theme_index = max(themes.find(init_active_theme_name), 0) # if theme was deleted, then set active index to 0
-            scene.theme_switcher.active_theme_index = init_active_theme_index
-        return {'FINISHED'}
-        
-    @staticmethod
-    def get_xmls(scene, dir_path):
-        xml_names = os.listdir(dir_path)
-        xmls = [(xml_name, os.path.join(dir_path, xml_name)) for xml_name in xml_names]
-        return xmls
-            
-    @staticmethod
-    def import_themes(scene, xmls):
-        for xml_name, xml_path in xmls:
-            theme = scene.theme_switcher.themes.add()
-            theme.path = xml_path
-            
-            name = xml_name.replace("_", " ")
-            if name.endswith(".xml"):
-                name = name[:-4]
-            theme.name = name.title() # CamelCase
-            
 
-class TSWITH_OT_edit(Operator):
+    def invoke(self, context, event):
+        addon_prefs = context.preferences.addons[__name__].preferences
+
+        # Backup active theme name.
+        if addon_prefs.themes:
+            idx = addon_prefs.active_theme_index
+            init_active_theme_name = addon_prefs.themes[idx].name
+        else:
+            init_active_theme_name = None
+
+        # Clear theme collection.
+        addon_prefs.themes.clear()
+
+        # Get theme xmls.
+        xmls = collect_xmls()
+
+        # Add themes to collection.
+        import_themes(xmls)
+
+        # Restore active theme or reset index.
+        active_theme_index = 0
+        if init_active_theme_name is not None:
+            # If current active theme was deleted set active index to 0.
+            init_active_theme_index = addon_prefs.themes.find(init_active_theme_name)
+            active_theme_index = max(init_active_theme_index, 0)
+
+        addon_prefs.active_theme_index = active_theme_index
+        return {'FINISHED'}
+
+
+def collect_xmls() -> list[tuple[str, str]]:
+    """Get xml files in directory path and return their names and paths."""
+    theme_dir_paths = bpy.utils.preset_paths("interface_theme")
+    xmls = []
+    for dir_path in theme_dir_paths:
+        xml_names = os.listdir(dir_path)
+        xmls.extend([(xml_name, os.path.join(dir_path, xml_name)) for xml_name in xml_names])
+    xmls.sort()
+    return xmls
+
+
+def import_themes(xmls: list[tuple[str, str]]) -> None:
+    """Fill addon properties with names and paths of xmls."""
+    addon_prefs = bpy.context.preferences.addons[__name__].preferences
+
+    for xml_name, xml_path in xmls:
+        theme = addon_prefs.themes.add()
+        theme.path = xml_path
+
+        # Underscores to whitespaces.
+        name = xml_name.replace("_", " ")
+        # CamelCase.
+        name = name.title()
+        # Remove extension.
+        if name.endswith(".Xml"):
+            name = name[:-4]
+        theme.name = name
+
+
+class TSWITCH_OT_edit(Operator):
     bl_idname = "theme_switcher.edit"
     bl_label = "Edit Themes"
     bl_description = "Open theme editor"
     bl_options = {'INTERNAL'}
-            
+
     def invoke(self, context, event):
         context.preferences.active_section = 'THEMES'
         bpy.ops.screen.userpref_show('INVOKE_DEFAULT')
         return {'FINISHED'}
 
 
-class TSWITH_PT_sidebar(Panel):
+class TSWITCH_PT_sidebar(Panel):
     bl_label = "Theme"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
@@ -112,63 +127,100 @@ class TSWITH_PT_sidebar(Panel):
 
     def draw(self, context):
         layout = self.layout
-        scene = context.scene
+        addon_prefs = context.preferences.addons[__name__].preferences
 
-        row = layout.row()
-        row.template_list("TSWITCH_UL_themes", "", scene.theme_switcher, "themes", scene.theme_switcher, "active_theme_index")
+        col = layout.column(align=True)
+        col.template_list("TSWITCH_UL_themes", "", addon_prefs, "themes", addon_prefs, "active_theme_index")
 
-        sub = row.column(align=True)
-        sub.operator("theme_switcher.reload", icon='FILE_REFRESH', text="")
-        sub.operator("theme_switcher.edit", icon='COLOR', text="")
+        row = col.row(align=True)
+        row.operator("theme_switcher.reload", icon='FILE_REFRESH', text="Reload")
+        row.operator("theme_switcher.edit", icon='COLOR', text="Edit")
 
 
-def activate_theme(self, context):
-    scene = context.scene
-    active_theme_index = scene.theme_switcher.active_theme_index
-    active_theme_path = scene.theme_switcher.themes[active_theme_index].path
-    
+def activate_theme(_, context):
+    addon_prefs = context.preferences.addons[__name__].preferences
+    active_theme_index = addon_prefs.active_theme_index
+    active_theme_path = addon_prefs.themes[active_theme_index].path
+
     bpy.ops.script.execute_preset(filepath=active_theme_path, menu_idname="USERPREF_MT_interface_theme_presets")
 
 
-class TSWITH_PG_themes(PropertyGroup):
+class TSWITCH_PG_themes(PropertyGroup):
     # name = StringProperty() -> Instantiated by default
     path: StringProperty()
 
 
-class TSWITH_PG_scene(PropertyGroup):
-    # name = StringProperty() -> Instantiated by default
-    themes: CollectionProperty(type=TSWITH_PG_themes)
+def update_sidebar_category(self, _):
+    is_panel = hasattr(bpy.types, 'TSWITCH_PT_sidebar')
+    if is_panel:
+        try:
+            bpy.utils.unregister_class(TSWITCH_PT_sidebar)
+        except:  # noqa
+            pass
+    TSWITCH_PT_sidebar.bl_category = self.sidebar_category
+    bpy.utils.register_class(TSWITCH_PT_sidebar)
+
+
+class TSWITCH_preferences(AddonPreferences):
+    bl_idname = __name__
+
+    themes: CollectionProperty(
+        type=TSWITCH_PG_themes,
+    )
     active_theme_index: IntProperty(
-        name="Active Index", 
+        name="Active Index",
         description="Index of the currently active theme",
-        default=0, 
-        update=activate_theme)
+        default=0,
+        update=activate_theme,
+    )
+    sidebar_category: StringProperty(
+        name="Sidebar Category",
+        description="Name for the tab in the sidebar panel",
+        default="View",
+        update=update_sidebar_category,
+    )
+
+    def draw(self, _):
+        layout = self.layout
+
+        col = layout.column()
+        col.use_property_split = True
+        col.use_property_decorate = False
+
+        col.prop(self, "sidebar_category")
 
 
 classes = (
     TSWITCH_UL_themes,
-    TSWITH_PT_sidebar,
-    TSWITH_OT_reload,
-    TSWITH_OT_edit,
-    TSWITH_PG_themes,
-    TSWITH_PG_scene
+    TSWITCH_PT_sidebar,
+    TSWITCH_OT_reload,
+    TSWITCH_OT_edit,
+    TSWITCH_PG_themes,
+    TSWITCH_preferences,
 )
 
 
+def initialize_themes():
+    addon_prefs = bpy.context.preferences.addons[__name__].preferences
+
+    if not addon_prefs.themes:
+        # Get theme xmls.
+        xmls = collect_xmls()
+
+        # Add themes to collection.
+        import_themes(xmls)
+
+
 def register():
-    from bpy.utils import register_class
     for cls in classes:
         register_class(cls)
-    
-    Scene.theme_switcher = PointerProperty(type=TSWITH_PG_scene)
+
+    initialize_themes()
 
 
 def unregister():
-    from bpy.utils import unregister_class
     for cls in reversed(classes):
         unregister_class(cls)
-
-    del Scene.theme_switcher
 
 
 if __name__ == "__main__":
